8e6f1da76f27d0bf284382d3d0e6c06d5903b31f
osarisky <ondrej2451@gmail.com>
2018-11-27 13:29:17 +0100

    TicsDataDetail  rename

--- webapp/controller/TicsDataDetail.controller.js
+++ webapp/controller/TicsDataDetail.controller.js
@@ -3,7 +3,7 @@ sap.ui.define([
 ], function(Controller) {
 	"use strict";
 
-	return Controller.extend("TiCS.controller.TaskDataDetail", {
+	return Controller.extend("TiCS.controller.TicsDataDetail", {
 
 		/**
 		 * Called when a controller is instantiated and its View controls (if available) are already created.
