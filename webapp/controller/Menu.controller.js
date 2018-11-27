9b7a6f3b7984ed26394eb2806655afa13cd9c69f
osarisky <ondrej2451@gmail.com>
2018-11-27 13:07:42 +0100

    Tics Data view + Rename

--- webapp/controller/Menu.controller.js
+++ webapp/controller/Menu.controller.js
@@ -35,7 +35,7 @@ sap.ui.define([
 			}	
 
 			if (id.includes("__item2")) {
-				oRouter.navTo("TaskData");
+				oRouter.navTo("TicsData");
 			}	
 
 			if (id.includes("__item3")) {
--- webapp/controller/TicsData.controller.js
+++ webapp/controller/TicsData.controller.js
@@ -3,7 +3,7 @@ sap.ui.define([
 ], function(Controller) {
 	"use strict";
 
-	return Controller.extend("TiCS.controller.TaskData", {
+	return Controller.extend("TiCS.controller.TicsData", {
 
 		/**
 		 * Called when a controller is instantiated and its View controls (if available) are already created.
