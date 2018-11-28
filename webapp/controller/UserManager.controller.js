2ddcbfd160399afe226e6f447d15cf692e67e638
brejano <brejano@gmail.com>
2018-11-28 10:26:39 +0100

    Project, Users

--- webapp/controller/UserManager.controller.js
+++ webapp/controller/UserManager.controller.js
@@ -14,7 +14,13 @@ sap.ui.define([
 			var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
 			oRouter.navTo("UserDetail");
 
-		},	
+		},
+			onDeleteClick: function() {
+
+		},
+	onEditClick: function() {
+
+		},
 		onInit: function() {
 		
 			}
