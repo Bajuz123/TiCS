4672fe59416de1cd71888818c322fdada2b9ed92
bajuz123 <stoma1234@gmail.com>
2018-11-29 19:09:29 +0100

    login

--- webapp/controller/Login.controller.js
+++ webapp/controller/Login.controller.js
@@ -4,7 +4,9 @@ sap.ui.define([
 	"use strict";
 
 	return Controller.extend("TiCS.controller.Login", {
-
+		onLoginClick:function(){
+			
+		}
 		/**
 		 * Called when a controller is instantiated and its View controls (if available) are already created.
 		 * Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
