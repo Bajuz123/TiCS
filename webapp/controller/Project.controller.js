4cd6a0c076f813a75bcaaf63e11a6a993b32f8eb
osarisky <ondrej2451@gmail.com>
2018-11-29 12:39:57 +0100

    fragments

--- webapp/controller/Project.controller.js
+++ webapp/controller/Project.controller.js
@@ -4,7 +4,7 @@ sap.ui.define([
 	"use strict";
 
 	var selProject = {projektnummer: "", beschreibung: ""};
-var fragProject = {};
+var fragProject;
 	return Controller.extend("TiCS.controller.Project", {
 		onItemPress: function(oEvent) {
 			selProject.projektnummer = oEvent.getParameter("listItem").getBindingContext("tics").getProperty("projektnummer") ;
@@ -30,15 +30,53 @@ var fragProject = {};
 		},
 
 		onAddClick: function() {
+		//	var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+		//	oRouter.navTo("ProjectDetail");
+		if (!fragProject) {
+    		 fragProject  = new sap.ui.xmlfragment("TiCS.view.ProjectDetail", this.oView.getController() );
+             this.oView.addDependent(fragProject);
+    	}
+        	fragProject.open();
+		},
+			onOKClick: function() {
+			var oEntry = {};
+
+			var resourceModel = this.oView.getModel("i18n");
+			oEntry.projektnummer = this.oView.byId("__inputProject").getValue();
+			oEntry.beschreibung = this.oView.byId("__inpuProjectDesc").getValue();
+
+			var oModelRegTest = this.oView.getModel("tics");
+
+			var oModel = sap.ui.getCore().getModel("SelectedProject");
+			if (typeof oModel !== 'undefined') {
+				var selProject = oModel.getData("selProject");
+				if (typeof selProject !== 'undefined') {
+					var editOKTxt = resourceModel.getProperty("EditOK");
+					var editFailTxt = resourceModel.getProperty("EditFail");
+	
+					oModelRegTest.update("/PROJECT_SET(projektnummer='" + oEntry.projektnummer + "')", oEntry, {
+						success : function(data) {
+							sap.m.MessageToast.show(editOKTxt);
+						},
+						error : function(e) {
+							sap.m.MessageToast.show(editFailTxt);
+						}
+					});
+				}
+			} else {
+				oModelRegTest.create("/PROJECT_SET", oEntry);
+				var resourceModel = this.getView().getModel("i18n");
+				var addOKTxt = resourceModel.getProperty("ProjectAddOK");
+				sap.m.MessageToast.show(addOKTxt);
+			}
+
+			oModelRegTest.refresh();
 			var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
-			oRouter.navTo("ProjectDetail");
-	//	var oView = sap.ui.getCore().byId("__xmlview2");
-		
-		//	if (fragProject) {
-    	//	 fragProject  = sap.ui.xmlfragment("TiCS.view.ProjectDetail", oView.getController() );
-           //  oView.addDependent(fragProject);
-    	//	}
-        //	fragProject.open();
+			oRouter.navTo("Project");
+		},
+
+		onCancelClick: function() {
+		fragProject.close();
 		},
 
 		onDeleteClick: function() {
