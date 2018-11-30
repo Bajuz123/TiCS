d56207c642198c93f2d95270d5d071faf7c5477e
bajuz123 <stoma1234@gmail.com>
2018-11-30 09:40:17 +0100

    model update after authentif

--- webapp/controller/Login.controller.js
+++ webapp/controller/Login.controller.js
@@ -28,44 +28,16 @@ sap.ui.define([
 		},
 
 		successApproval: function() {
-			sap.m.MessageToast.show("ok");
+			var oUserModel = sap.ui.getCore().getModel("User");
+			var user = oUserModel.getData("user");
+			user.authentificated = true;
+			sap.ui.getCore().setModel(oUserModel, "User");
+			sap.m.MessageToast.show("Authentificated");
 		},
 
 		errorApproval: function() {
-			sap.m.MessageToast.show("fail");
+			sap.m.MessageToast.show("Authentification fail");
 		}
-
-		/**
-		 * Called when a controller is instantiated and its View controls (if available) are already created.
-		 * Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
-		 * @memberOf TiCS.view.Login
-		 */
-		/**
-		 * Similar to onAfterRendering, but this hook is invoked before the controller's View is re-rendered
-		 * (NOT before the first rendering! onInit() is used for that one!).
-		 * @memberOf TiCS.view.Login
-		 */
-		//	onBeforeRendering: function() {
-		//
-		//	},
-
-		/**
-		 * Called when the View has been rendered (so its HTML is part of the document). Post-rendering manipulations of the HTML could be done here.
-		 * This hook is the same one that SAPUI5 controls get after being rendered.
-		 * @memberOf TiCS.view.Login
-		 */
-		//	onAfterRendering: function() {
-		//
-		//	},
-
-		/**
-		 * Called when the Controller is destroyed. Use this one to free resources and finalize activities.
-		 * @memberOf TiCS.view.Login
-		 */
-		//	onExit: function() {
-		//
-		//	}
-
 	});
 
 });
