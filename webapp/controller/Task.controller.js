488325f8bf647ba03bf458031f244e8b75f4ccc4
osarisky <ondrej2451@gmail.com>
2019-01-14 14:16:07 +0100

    Function 4 Task

--- webapp/controller/Task.controller.js
+++ webapp/controller/Task.controller.js
@@ -52,11 +52,50 @@ sap.ui.define([
 		},
 
 		onDeleteClick: function() {
+			var resourceModel = this.getView().getModel("i18n");
+			var modelTics = this.oView.getModel("tics");
+			//			var oTable = this.getView().byId("__tableProjects");
+			var deleteSelectText = resourceModel.getProperty("DeleteSelectFail");
+			var deleteOKText = resourceModel.getProperty("DeleteOK");
+			var deleteFailText = resourceModel.getProperty("DeleteFail");
 
+			if (selTask.id !== "") {
+				var user = this.getView().getModel("User");
+
+				modelTics.remove("/TASK_SET(id='" + selTask.id + "')", {
+					method: "DELETE",
+					urlParameters: {
+						"username": user.username,
+						"password": user.password
+					},
+					success: function() {
+						sap.m.MessageToast.show(deleteOKText);
+					},
+					error: function() {
+						sap.m.MessageToast.show(deleteFailText);
+					}
+				});
+				modelTics.refresh();
+				this.clearSelected();
+			} else {
+				sap.m.MessageToast.show(deleteSelectText);
+			}
 		},
 
 		onEditClick: function() {
+			var resourceModel = this.getView().getModel("i18n");
+			//			var oTable = this.getView().byId("__tableProjects");
+			var editSelectText = resourceModel.getProperty("EditSelectFail");
 
+			if (selTask.id !== "") {
+				var oModel = this.getView().getModel("SelectedProject");
+				selTask.method = "update";
+				oModel.setData(selTask);
+				this.getView().setModel(oModel, "SelectedTask");
+				this.openFragUser();
+			} else {
+				sap.m.MessageToast.show(editSelectText);
+			}
 		},
 
 		openFragUser: function() {
@@ -82,49 +121,49 @@ sap.ui.define([
 			this.clearSelected();
 			fragTask.close();
 		},
-		
+
 		onOKClick: function() {
-			var resourceModel = this.getView().getModel("i18n");
-			var editOKTxt = resourceModel.getProperty("EditOK");
-			var editFailTxt = resourceModel.getProperty("EditFail");
-			var addOKTxt = resourceModel.getProperty("TasktAddOK");
-			var oEntry = {};
-			var oModel = this.getView().getModel("SelectedTask");
-			var modelTics = this.oView.getModel("tics");
-			if (typeof oModel !== "undefined") {
-				selTask = oModel.getData("selTask");
-				if (typeof selTask !== "undefined") {
-					oEntry.aufgabe = selTask.aufgabe;
-					oEntry.beschreibung = selTask.beschreibung;
+				var resourceModel = this.getView().getModel("i18n");
+				var editOKTxt = resourceModel.getProperty("EditOK");
+				var editFailTxt = resourceModel.getProperty("EditFail");
+				var addOKTxt = resourceModel.getProperty("TasktAddOK");
+				var oEntry = {};
+				var oModel = this.getView().getModel("SelectedTask");
+				var modelTics = this.oView.getModel("tics");
+				if (typeof oModel !== "undefined") {
+					selTask = oModel.getData("selTask");
+					if (typeof selTask !== "undefined") {
+						oEntry.aufgabe = selTask.aufgabe;
+						oEntry.beschreibung = selTask.beschreibung;
 
-					var oUserModel = this.getView().getModel("User");
-					oEntry.username = oUserModel.username;
-					oEntry.password = oUserModel.password;
-					if (selTask.method !== "create") {
+						var oUserModel = this.getView().getModel("User");
+						oEntry.username = oUserModel.username;
+						oEntry.password = oUserModel.password;
+						if (selTask.method !== "create") {
 
-						modelTics.update("/TASK_SET(projektnummer='" + oEntry.aufgabe + "')", oEntry, {
-							success: function() {
-								sap.m.MessageToast.show(editOKTxt);
-							},
-							error: function() {
-								sap.m.MessageToast.show(editFailTxt);
-							}
-						});
-					} else if (selTask.method === "create") {
-						modelTics.create("/TASK_SET", oEntry);
-						sap.m.MessageToast.show(addOKTxt);
+							modelTics.update("/TASK_SET(projektnummer='" + oEntry.aufgabe + "')", oEntry, {
+								success: function() {
+									sap.m.MessageToast.show(editOKTxt);
+								},
+								error: function() {
+									sap.m.MessageToast.show(editFailTxt);
+								}
+							});
+						} else if (selTask.method === "create") {
+							modelTics.create("/TASK_SET", oEntry);
+							sap.m.MessageToast.show(addOKTxt);
+						}
 					}
 				}
+				modelTics.refresh();
+				this.clearSelected();
+				fragTask.close();
 			}
-			modelTics.refresh();
-			this.clearSelected();
-			fragTask.close();
-		}
-		/**
-		 * Called when a controller is instantiated and its View controls (if available) are already created.
-		 * Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
-		 * @memberOf TiCS.view.view.Task
-		 */
+			/**
+			 * Called when a controller is instantiated and its View controls (if available) are already created.
+			 * Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
+			 * @memberOf TiCS.view.view.Task
+			 */
 
 		/**
 		 * Similar to onAfterRendering, but this hook is invoked before the controller's View is re-rendered
@@ -132,7 +171,21 @@ sap.ui.define([
 		 * @memberOf TiCS.view.view.Task
 		 */
 		//	onBeforeRendering: function() {
+		//	var oUserModel = this.getView().getModel("User");
+		//	var isValid = this.isUserValid(oUserModel);
+		//	if ((isValid === true)||(isValid === "true")) {
+		//		var oModel = new sap.ui.model.json.JSONModel();
+		//		oModel.setData(selTask);
+		//		this.getView().setModel(oModel, "SelectedTask");
 		//
+		//		var oDataModel = this.getView().getModel("tics");
+		//		oDataModel.read("/TASK_SET", {
+		//			error: function(e) {
+		//				sap.m.MessageToast.show(e);
+		//			}
+		//		});
+		//		this.createUserFilter(oUserModel);
+		//	}
 		//	},
 
 		/**
